53f5df48807606ecd87192aabb57e85fcd71820c
David Hartmann <david.hartmann1114@gmail.com>
2023-10-18 04:43:37 -0400

    update

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -407,7 +407,7 @@ function useUpdate() {
     timerId = (setTimeout(() => {
       setUpdate(prev => prev + 1)
       setLoading(false)
-    }, 2000) as unknown) as number
+    }, 10000) as unknown) as number
   }
 
   useEffect(() => {
